677c79b49055a58c516f518ff6a955fd91e29ed0
Kristina Yancer <kristyjanzer.work@gmail.com>
2025-03-25 15:53:08 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,16 @@
-$(function() {
+// Burger Menu
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const burgerMenu = document.querySelector(".header .header-button-menu");
+const burgerBody = document.querySelector(".header .header-button-menu__body");
 
+
+burgerMenu.addEventListener("click", (e) => {
+  header.classList.toggle("header-menu-open");
+  burgerBody.classList.toggle("active");
+  body.classList.toggle("lock");
+  e.stopPropagation();
 });
 
+
+$(".main-menu").clone().appendTo(".header-button-menu__body");
